b21b68dc12d54d8195f4827638ea580809811b4a
peter <peterlimail47@gmail.com>
2021-11-11 17:47:10 -0800

    saving

--- cards.py
+++ cards.py
@@ -98,7 +98,7 @@ ranger_deck.add_card(
 
 misc_deck.add_card(
     "Status: Slowed",
-    "This target has reduced movement speed, all movement is moved by half."
+    "This target has reduced movement speed, all movement is cut in half."
 )
 
 ranger_deck.add_card(
@@ -114,17 +114,18 @@ misc_deck.add_card(
 
 speedy_deck.add_card(
     "Aimed Strike",
-    "Melee: Deal 3 + 1d6 damage"
+    "Melee: Deal 3 + 1d6 damage, deal an extra 1d4 if attacking from "
+    "the flank"
 )
 
 speedy_deck.add_card(
     "Parting Strike",
-    "Melee: Deal 1 + 1d3 damage, move 2"
+    "Melee: Deal 1 + 1d3 damage, Move 2"
 )
 
 speedy_deck.add_card(
     "Blur",
-    "Grant self 1 + 1d2 [Blur]"
+    "Gain 1 + 1d2 [Blur]"
 )
 
 misc_deck.add_card(
@@ -134,5 +135,49 @@ misc_deck.add_card(
 
 speedy_deck.add_card(
     "Shuriken",
-    "Range 2: Deal 3 + 1d3 damage"
+    "Range 2: Deal 3 + 1d3 damage, deal an extra 1d4 if attacking "
+    "from the flank"
+)
+
+# ----------- tanky cards ------------
+
+tanky_deck.add_card(
+    "Heavy Strike",
+    "Melee: Deal 4 + 1d6 damage"
+)
+
+tanky_deck.add_card(
+    "Shields Up!",
+    "Gain 4 + 1d4 block"
+)
+
+tanky_deck.add_card(
+    "Get behind me!",
+    "All allies in my flank, take half damage for 1 turn.",
+)
+
+tanky_deck.add_card(
+    "Sunder arms",
+    "Melee: Deal 2 damage, target has card draw reduced by 2 for the next turn.",
+)
+
+# ------------- Support cards --------------
+support_deck.add_card(
+    "Energy Bolt",
+    "Ranged 2: Deal 2 + 1d4 damage"
+)
+
+support_deck.add_card(
+    "Mending Bolt",
+    "Ranged 2: Heal for 2 + 1d4"
+)
+
+support_deck.add_card(
+    "Motivate",
+    "Ranged 2: target's next activation will draw 3 extra cards."
+)
+
+support_deck.add_card(
+    "Stamina Surge",
+    "Range 2: target's movement increases by 2 for 2 turns."
 )
