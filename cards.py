6a257453d4fbd6c14d8899fedfe4d3105acdc09d
peter <peterlimail47@gmail.com>
2021-11-12 20:48:12 -0800

    added new compositing system and support for flavour text

--- cards.py
+++ cards.py
@@ -11,6 +11,7 @@ class Deck:
     def __init__(self, deck_name):
         self.deck_name = deck_name
         self.wrapper = textwrap.TextWrapper(width=37)
+        self.card_infos = []
 
     def defix_links(self, text):
         new_text = ""
@@ -50,7 +51,7 @@ class Deck:
 
         return new_text
 
-    def add_card(self, card_name, text):
+    def add_card(self, card_name, text, decks=None, flavour=None):
         global card_infos
         splitlines = text.split('\n\n')
         line_count = 0
@@ -66,14 +67,16 @@ class Deck:
         line_count -= 1
 
         text_inner = "\n \n".join(segments)
-        card_infos.append(
-            {
-                "deck": self.deck_name,
+        card_info = {
+                "decks": [self.deck_name],
                 "name": card_name,
                 "text": text_inner,
                 "line_count": line_count,
             }
-        )
+        if flavour is not None:
+            card_info['flavour'] = flavour
+        card_infos.append(card_info)
+        self.card_infos.append(card_info)
 
 ranger_deck = Deck('ranger')
 tanky_deck = Deck('tanky')
@@ -84,100 +87,120 @@ misc_deck = Deck('misc')
 ranger_deck.add_card(
     "Sharp Shot", 
     "Range 4: Deal 3 + 1d4 Damage.",
+    flavour = '"Take aim and pull, let the shot come as a surprise."',
 )
 
 ranger_deck.add_card(
     "First Aid!", 
-    "Range self: Heal for 3 + 1d3 health."
+    "Range self: Heal for 3 + 1d3 health.",
+    flavour="The first thing any ranger learns is how to take care of themself."
 )
 
 ranger_deck.add_card(
     "Binding Shot", 
-    "Range 4: Deal 2 damage, the target is [slowed] for their next turn."
+    "Range 4: Deal 2 damage, the target is [slowed] for their next turn.",
+    flavour="Quite simple, you can fire arrows with ropes on them"
 )
 
 misc_deck.add_card(
     "Status: Slowed",
-    "This target has reduced movement speed, all movement is cut in half."
+    "This target has reduced movement speed, all movement is cut in half.",
+    flavour="Slow and steady might win the race but it rarely wins any fights."
 )
 
+bomb_trap_dmg = '4 + d4'
 ranger_deck.add_card(
     "Place Bomb trap", 
-    "Range 1: Place a 2 + d4 damage [Bomb Trap] into an empty square."
+    f"Range 1: Place a {bomb_trap_dmg} damage [Bomb Trap] into an empty square.",
+    flavour="gunpowder, rope, a pin, and a splash of wild magic."
 )
 
 misc_deck.add_card(
     "Contraption: Bomb Trap",
     "Detonates when an enemy steps onto this square or an adjacent square."
-    "\n\nDeals 4 + d4 damage to all characters in range.\nAllies take half damage."
+    f"\n\nDeals {bomb_trap_dmg} damage to all characters in range.\nAllies take half damage.",
+    flavour="A bit of a lost art, making bombs. I wonder why they outlawed it."
 )
 
 speedy_deck.add_card(
     "Aimed Strike",
     "Melee: Deal 3 + 1d6 damage, deal an extra 1d4 if attacking from "
-    "the flank"
+    "the flank",
+    flavour="Cut them from behind, it'll hurt more.",
 )
 
 speedy_deck.add_card(
     "Parting Strike",
-    "Melee: Deal 1 + 1d3 damage, Move 2"
+    "Melee: Deal 1 + 1d3 damage, Move 2",
+    flavour="Strike! then vanish."
 )
 
 speedy_deck.add_card(
     "Blur",
-    "Move 2, Gain 1 + 1d2 [Blur]"
+    "Move 2, Gain 1 + 1d2 [Blur]",
+    flavour="Whoosh!"
 )
 
 misc_deck.add_card(
     "Blurry",
-    "Blocks damage, blur stacks and is persistent."
+    "Blocks damage, blur stacks and is persistent.",
+    flavour="Can't hit what you can't hit."
 )
 
 speedy_deck.add_card(
     "Shuriken",
     "Range 2: Deal 3 + 1d3 damage, deal an extra 1d4 if attacking "
-    "from the flank"
+    "from the flank",
+    flavour="It's not always a throwing star. Sometimes it's sharp rocks, or a bucket."
 )
 
 # ----------- tanky cards ------------
 
 tanky_deck.add_card(
     "Heavy Strike",
-    "Melee: Deal 3 + 1d6 damage, ends turn."
+    "Melee: Deal 3 + 1d6 damage, ends turn.",
+    flavour="\"SMASH!\"- some green dude once."
 )
 
 tanky_deck.add_card(
     "Shields Up!",
-    "Gain 4 + 1d4 block"
+    "Gain 4 + 1d4 block",
+    flavour="\"Bet you can't hurt me now bro!\" - local man, moments before disaster."
 )
 
 tanky_deck.add_card(
     "Get behind me!",
-    "All allies in my flank, take half damage for 1 turn.",
+    "All allies in my flank,\ntake half damage for 1 turn.",
+    flavour="\"Aw geez, We better do what he says Rick.\""
 )
 
 tanky_deck.add_card(
     "Sunder arms",
     "Melee: Deal 2 damage, target has card draw reduced by 2 for the next turn.",
+    flavour="This technique was developed in order to win arm wrestling competitions."
 )
 
 # ------------- Support cards --------------
 support_deck.add_card(
     "Energy Bolt",
-    "Ranged 2: Deal 2 + 1d4 damage"
+    "Ranged 2: Deal 2 + 1d4 damage",
+    flavour="This won't hurt a bit... no seriously this really wont hurt."
 )
 
 support_deck.add_card(
     "Mending Bolt",
-    "Ranged 2: Heal for 2 + 1d4"
+    "Ranged 2: Heal for 2 + 1d4",
+    flavour="\"Na na na na Poly! na na na Sporin!\""
 )
 
 support_deck.add_card(
     "Motivate",
-    "Ranged 2: target's next activation will draw 3 extra cards."
+    "Ranged 2: target's next activation will draw 3 extra cards.",
+    flavour="Oh damn someone cute's watching, don't mess up."
 )
 
 support_deck.add_card(
     "Stamina Surge",
-    "Range 2: target's movement increases by 2 for 2 turns."
+    "Range 2: target's movement increases by 2 for 2 turns.",
+    flavour="\"Oh my god he just ran right in.\"- local man, moments before disaster."
 )
--- main.py
+++ main.py
@@ -12,11 +12,24 @@ blanks = {
     "misc": "info_blank.png",
 }
 
+cardbase = "cardblanks\CardBlank.png"
+
+icons = {
+    "ranger": "ranger_icon.png",
+    "speedy": "rogue_icon.png",
+    "support": "support_icon.png",
+    "tanky": "tanky_icon.png",
+    "magus": "magus_icon.png",
+    "misc": "info_icon.png",
+}
+
+
 class CardMaker:
     def __init__(self):
         self.title_height = 48
         self.text_height = 36
         self.text_height_smaller = 32
+        self.text_height_flavor = 20
         self.title_font = ImageFont.truetype('Charter Regular.ttf', size=self.title_height)
         self.text_font = ImageFont.truetype('Charter Regular.ttf', size=self.text_height)
         self.text_font_smaller = ImageFont.truetype('Charter Regular.ttf', size=self.text_height_smaller)
@@ -24,7 +37,17 @@ class CardMaker:
         self.text_fonts = {
             "normal": (36, ImageFont.truetype('Charter Regular.ttf', size=36)),
             "small": (30, ImageFont.truetype('Charter Regular.ttf', size=30)),
+            "flavour": (self.text_height_flavor, ImageFont.truetype('Charter Italic.ttf', size=self.text_height_flavor)),
         }
+
+    def get_text_dimensions(self, text_string, font):
+        # https://stackoverflow.com/a/46220683/9263761
+        ascent, descent = font.getmetrics()
+
+        text_width = font.getmask(text_string).getbbox()[2]
+        text_height = font.getmask(text_string).getbbox()[3] + descent
+
+        return (text_width, text_height)
 
     def make_card_texture(self, card_info, out_dir=None):
         if not out_dir:
@@ -32,15 +55,33 @@ class CardMaker:
 
         text_height, text_font = self.text_fonts['normal']
 
-        if(card_info['line_count'] > 5):
+        if(card_info['line_count'] > 4):
             text_height, text_font = self.text_fonts['small']
 
-        src_image = Image.open(blanks[card_info['deck']])
+        src_image = Image.open(cardbase)
         width, height = src_image.size
 
         out_image = ImageDraw.Draw(src_image)
+
+        # draw the name
         out_image.text((width/2, self.title_height*1.5), card_info["name"], font=self.title_font, anchor='ms')
 
+        # draw the costs.
+
+        # draw class icons
+        icon_name = card_info['decks'][0]
+        icon_image = Image.open("cardblanks/" + icons[icon_name])
+        icon_image = icon_image.convert("RGBA")
+        icon_image_data = icon_image.getdata()
+        icon_image_new_data = []
+
+        for pix in icon_image_data:
+            icon_image_new_data.append((pix[0], pix[1], pix[2], int(pix[3] * 0.15)))
+
+        icon_image.putdata(icon_image_new_data)
+        icon_width, icon_height = icon_image.size
+
+        # start drawing the text height
         desc_box_startx = 43
         desc_box_endx = 694
         desc_box_width = desc_box_endx - desc_box_startx
@@ -50,12 +91,30 @@ class CardMaker:
         desc_box_height = desc_box_endy - desc_box_starty
 
         desc_box_lmargin = 12
-        card_text_height = card_info['line_count'] * text_height
+        card_text_height = card_info['line_count'] * (text_height + 16)
         assert card_text_height < desc_box_height, "Damn dude your description is really long"
             
         card_text_top_margin = (desc_box_height - card_text_height)/2
 
-        out_image.multiline_text((desc_box_lmargin + desc_box_startx, card_text_top_margin+desc_box_starty), card_info["text"], font=text_font, align='center', spacing=12)
+        # finish drawing the image
+        icon_offset = (
+            int((desc_box_width - icon_width)/2 + desc_box_startx), #x
+            int((desc_box_height - icon_height)/2 + desc_box_starty), #y
+        )
+        src_image.paste(icon_image, icon_offset, icon_image)
+
+        # draw the text
+        out_image.multiline_text((desc_box_lmargin + desc_box_startx, card_text_top_margin + desc_box_starty), card_info["text"], font=text_font, align='center', spacing=12)
+
+        # draw the flavor text
+        if "flavour" in card_info:
+            flavour_font_height, flavour_font = self.text_fonts['flavour']
+            flavour_width, flavour_height = self.get_text_dimensions(card_info['flavour'], flavour_font)
+            flavour_offset = desc_box_endx - 12 - flavour_width, desc_box_endy - flavour_height*2 - 12
+            out_image.multiline_text(flavour_offset, card_info['flavour'], font=flavour_font)
+
+
+        # write out the card
 
         os.makedirs(out_dir, exist_ok=True)
 
@@ -64,7 +123,7 @@ class CardMaker:
         filename = filename.replace(':', '_')
         filename = filename.replace('!', '_')
 
-        src_image.save(out_dir + f"{filename}.png")
+        src_image.save(out_dir + f"{card_info['decks'][0]}_{filename}.png")
 
     def make_all_cards(self, cards, out_dir):
         """makes all cards for the list of cards it is assumed that all the cards have unique names
@@ -75,7 +134,7 @@ class CardMaker:
 
 def test_make_aimed_shot():
     card_info = {
-        "deck": "ranger",
+        "decks": ["ranger"],
         "name": "Arrow Shot",
         "text": "Ranged 4: deal 3 + 1d4 Damage\n",
         "line_count": 1,
@@ -86,6 +145,7 @@ def test_make_aimed_shot():
 def test_make_all_cards():
     maker = CardMaker();
     maker.make_all_cards(cards.card_infos, 'playtest_cards/')
+
 
 if __name__ == "__main__":
     test_make_aimed_shot()
